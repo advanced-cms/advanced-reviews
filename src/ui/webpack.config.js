f914f890ea42209931ff9fd42e52f6181360f533
gregwiechec <gwiechec@o2.pl>
2019-03-11 21:34:50 +0100

    Added react component to project

---
+++ src/ui/webpack.config.js
@@ -0,0 +1,66 @@
+var path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: "./reviewComponent.tsx",
+  output: {
+    path: path.resolve(__dirname, ".\\dist"),
+    filename: "reviewComponent.bundle.js"
+  },
+  devtool: "eval-source-map",
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+          loader: "style-loader"
+        }, {
+          loader: "css-loader"
+        }, {
+          loader: "sass-loader",
+          options: {
+            includePaths: ["node_modules"]
+          }
+        }]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
+};
