abda963dba0355ad967b1324a6dd3571d51d3a38
gregwiechec <gwiechec@o2.pl>
2019-03-13 19:50:22 +0100

    Code cleanup on review dialog

--- src/ui/reviewEditorDialog.tsx
+++ src/ui/reviewEditorDialog.tsx
@@ -23,33 +23,27 @@ export default class ReviewDialog extends React.Component<ReviewDialogProps, any
 
   constructor(props: ReviewDialogProps) {
     super(props);
-    this.state = {value: 'Woof'};
+    this.state = { value: 'Woof' };
   }
-
-  
 
   render() {
     const { closeDialog, isDialogOpen, currentEditLocation } = this.props.reviewStore!;
+
+    const customAttribute = {
+      title: currentEditLocation.isDone ? "Uncheck to reopen the task" : "Mark task as done"
+    };
 
     return (
       <Dialog open={isDialogOpen} scrimClickAction="" escapeKeyAction="" onClose={closeDialog} >
         <DialogTitle>
           {currentEditLocation.propertyName}
-          <Checkbox nativeControlId='my-checkbox' 
-            title={currentEditLocation.isDone ? "Uncheck to reopen the task": "Mark task as done"} 
-            checked={currentEditLocation.isDone} onChange={(e) => currentEditLocation.isDone = e.target.checked }
-          />
+          <Checkbox nativeControlId='my-checkbox' {...customAttribute} checked={currentEditLocation.isDone}
+            onChange={(e) => currentEditLocation.isDone = e.target.checked} />
         </DialogTitle>
         <DialogContent>
           <div>
-            <TextField
-              label='Comment...'
-              textarea={true}
-              //onTrailingIconSelect={() => this.setState({ value: '' })}
-              //trailingIcon={<MaterialIcon role="button" icon="delete" />}
-            ><Input
-                value={this.state.value}
-                onChange={(e) => this.setState({ value: e.currentTarget.value })} />
+            <TextField label='Comment...' textarea={true}><Input value={this.state.value}
+              onChange={(e) => this.setState({ value: e.currentTarget.value })} />
             </TextField>
 
           </div>
