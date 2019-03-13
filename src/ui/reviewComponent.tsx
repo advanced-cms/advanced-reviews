8e51598d745df1995a9eecdf161bf4ba783d1bf2
gregwiechec <gwiechec@o2.pl>
2019-03-13 18:29:26 +0100

    Added mobx to project Added dialog to project

--- src/ui/reviewComponent.tsx
+++ src/ui/reviewComponent.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ReviewLocationsCollection from "./reviewLocationsCollection";
+import { stores } from "./reviewStore";
+import {Provider} from 'mobx-react';
 
-ReactDOM.render(<ReviewLocationsCollection />, document.getElementById("index"));
+//TODO: async
+stores.reviewStore.load();
+
+ReactDOM.render(<Provider {...stores}>
+    <ReviewLocationsCollection />
+</Provider>, document.getElementById("index"));
+
+
+//TODO: remove
+//https://medium.com/teachable/getting-started-with-react-typescript-mobx-and-webpack-4-8c680517c030
+//https://www.nealbuerger.com/2018/11/11/getting-started-with-mobx-5-and-typescript-3-react-16-6/
--- src/ui/reviewEditorDialog.tsx
+++ src/ui/reviewEditorDialog.tsx
@@ -1,45 +1,56 @@
 import React from "react";
-
+import { observer, inject } from 'mobx-react';
+import { IReviewComponentStore } from './reviewStore';
 import Dialog, {
-    DialogTitle,
-    DialogContent,
-    DialogFooter,
-    DialogButton,
-  } from '@material/react-dialog';
-  
-export default class ReviewDialog extends React.Component<any, any> {
-    constructor(props) {
-      super(props);
-      this.state = { isOpen: props.isOpen };
-    }
-  
-    componentDidUpdate(prevProps, prevState) {
-      if (this.state.isOpen === this.props.isOpen) {
-        return;
-      }
-      this.setState({
-        isOpen: this.props.isOpen
-      });
-    }
-  
-    onDialogClose(action) {
-      
-    }
-  
-    render() {
-      return (
-        <Dialog open={this.state.isOpen} scrimClickAction="" escapeKeyAction=""
-        onClose={this.onDialogClose.bind(this)}
-        >
-          <DialogTitle>My Dialog</DialogTitle>
-          <DialogContent>
-            <div>adasdasdas dasdas dasdas</div>
-          </DialogContent>
-          <DialogFooter>
-            <DialogButton action='dismiss'>Dismiss</DialogButton>
-            <DialogButton action='accept' isDefault>Accept</DialogButton>
-          </DialogFooter>
-        </Dialog>
-      );
-    }
+  DialogTitle,
+  DialogContent,
+  DialogFooter,
+  DialogButton,
+} from '@material/react-dialog';
+import TextField, { Input } from '@material/react-text-field';
+import '@material/react-dialog/index.scss';
+import '@material/react-text-field/index.scss';
+
+interface ReviewDialogProps {
+  reviewStore?: IReviewComponentStore
+}
+
+@inject('reviewStore')
+@observer
+export default class ReviewDialog extends React.Component<ReviewDialogProps, any> {
+
+  constructor(props: ReviewDialogProps) {
+    super(props);
+    this.state = {value: 'Woof'};
   }
+
+  
+
+  render() {
+    const { closeDialog, isDialogOpen, currentEditLocation } = this.props.reviewStore!;
+
+    return (
+      <Dialog open={isDialogOpen} scrimClickAction="" escapeKeyAction="" onClose={closeDialog} >
+        <DialogTitle>{currentEditLocation.propertyName}</DialogTitle>
+        <DialogContent>
+          <div>
+            <TextField
+              label='Comment...'
+              textarea={true}
+              //onTrailingIconSelect={() => this.setState({ value: '' })}
+              //trailingIcon={<MaterialIcon role="button" icon="delete" />}
+            ><Input
+                value={this.state.value}
+                onChange={(e) => this.setState({ value: e.currentTarget.value })} />
+            </TextField>
+
+          </div>
+        </DialogContent>
+        <DialogFooter>
+          <DialogButton action='dismiss'>Dismiss</DialogButton>
+          <DialogButton action='accept' isDefault>Accept</DialogButton>
+        </DialogFooter>
+      </Dialog>
+    );
+  }
+}
