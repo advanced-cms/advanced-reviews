d2f8baed235d5fcd861d0cdb972e2f303e1fe576
gregwiechec <gwiechec@o2.pl>
2019-03-15 22:54:06 +0100

    Create DDS based repository to store reviews

---
+++ src/approval-reviews/ApprovalReview.cs
@@ -0,0 +1,17 @@
+﻿using EPiServer.Core;
+using EPiServer.Data;
+using EPiServer.Data.Dynamic;
+
+namespace AdvancedApprovalReviews
+{
+    [EPiServerDataStore(AutomaticallyCreateStore = true, AutomaticallyRemapStore = true)]
+    public class ApprovalReview : IDynamicData
+    {
+        public Identity Id { get; set; }
+
+        [EPiServerDataIndex]
+        public ContentReference ContentLink { get; set; }
+
+        public string SerializedReview { get; set; }
+    }
+}
