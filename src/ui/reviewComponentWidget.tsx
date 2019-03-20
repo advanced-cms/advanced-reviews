8c0a21f11080fbf9dacfedce357a09aa423a1834
gregwiechec <gwiechec@o2.pl>
2019-03-20 20:45:17 +0100

    Load react component to EPiserver

---
+++ src/ui/reviewComponentWidget.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from 'mobx-react';
+import declare from "dojo/_base/declare";
+import WidgetBase from "dijit/_WidgetBase";
+import ReviewLocationsCollection from "./ReviewLocationsCollection";
+
+import { stores } from "./reviewStore";
+
+//TODO: async
+stores.reviewStore.load();
+
+export default declare([WidgetBase], {
+    postCreate: function () {
+        ReactDOM.render(<Provider {...stores}><ReviewLocationsCollection /></Provider>, this.domNode);
+    },
+    destroy: function () {
+        ReactDOM.unmountComponentAtNode(this.domNode);
+    }
+});
