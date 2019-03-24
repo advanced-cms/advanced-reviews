8d272d659eaab828ebf4d348bfd8d4151aa4fd99
gregwiechec <gwiechec@o2.pl>
2019-03-24 22:00:48 +0100

    scroll to latest comments

--- src/ui/reviewLocationsCollection.tsx
+++ src/ui/reviewLocationsCollection.tsx
@@ -2,45 +2,80 @@ import React from "react";
 import { observer, inject } from 'mobx-react';
 import "./Styles.scss";
 import classNames from "classnames";
-import { IReviewComponentStore } from './reviewStore';
+import { IReviewComponentStore, ReviewLocation } from './reviewStore';
 import ReviewEditorDialog from "./dialog/reviewEditorDialog";
 
 interface ReviewLocationCollectionProps {
   reviewStore?: IReviewComponentStore
 }
 
-@inject('reviewStore')
-@observer
-export default class ReviewLocationCollection extends React.Component<ReviewLocationCollectionProps, any> {
-
-private showReview(incrementBy: number) {
-  const {reviewLocations, dialog, closeDialog} = this.props.reviewStore!;
-  let reviewIndex = reviewLocations.indexOf(dialog.currentEditLocation) + incrementBy;
-  if (reviewIndex >= reviewLocations.length) {
-    reviewIndex = 0;
-  } else if (reviewIndex < 0) {
-    reviewIndex = reviewLocations.length - 1;
-  }
-  closeDialog("cancel");
-  dialog.showDialog(reviewLocations[reviewIndex]);
+interface ReviewLocationCollectionState {
+  isDialogOpen: boolean
 }
 
+@inject('reviewStore')
+@observer
+export default class ReviewLocationCollection extends React.Component<ReviewLocationCollectionProps, ReviewLocationCollectionState> {
+
+  constructor(props: ReviewLocationCollectionProps) {
+    super(props);
+    this.state = {
+      isDialogOpen: false
+    }
+  }
+
+  private showReview(incrementBy: number): void {
+    const { reviewLocations, dialog } = this.props.reviewStore!;
+    let reviewIndex = reviewLocations.indexOf(dialog.currentEditLocation) + incrementBy;
+    if (reviewIndex >= reviewLocations.length) {
+      reviewIndex = 0;
+    } else if (reviewIndex < 0) {
+      reviewIndex = reviewLocations.length - 1;
+    }
+    dialog.showDialog(reviewLocations[reviewIndex]);
+  }
+
+  showDialog(location: ReviewLocation): void {
+    const { dialog } = this.props.reviewStore!;
+    dialog.showDialog(location);
+    this.setState({
+      isDialogOpen: true
+    });
+  }
+
+onCloseDialog(action: string) : void {
+  if (action !== "save") {
+    this.setState({
+      isDialogOpen: false
+    });
+    return;
+}
+
+  const { saveDialog } = this.props.reviewStore!;
+  saveDialog();
+  this.setState({
+    isDialogOpen: false
+  });
+}
+  
   render() {
-    const {reviewLocations, dialog, currentItemIndex} = this.props.reviewStore!;
+    const { reviewLocations } = this.props.reviewStore!;
 
     return (
       <div>
         {reviewLocations.map(location => (
-          <div key={location.id} 
-          style={location.style}
-          title={location.formattedFirstComment} 
-          className={classNames("reviewLocation", { done: location.isDone, "new-comment": location.isUpdatedReview })}
-          onClick={() => dialog.showDialog(location)}>{location.id}</div>
+          <div key={location.id}
+            style={location.style}
+            title={location.formattedFirstComment}
+            className={classNames("reviewLocation", { done: location.isDone, "new-comment": location.isUpdatedReview })}
+            onClick={() => this.showDialog(location)}>{location.id}</div>
         ))}
 
-        <ReviewEditorDialog 
-            onPrevClick={() => this.showReview(-1)} 
-            onNextClick={() => this.showReview(1)}
+        <ReviewEditorDialog
+          isDialogOpen={this.state.isDialogOpen}
+          onPrevClick={() => this.showReview(-1)}
+          onNextClick={() => this.showReview(1)}
+          onCloseDialog={(action)=> this.onCloseDialog(action)}
         />
       </div>)
   };
