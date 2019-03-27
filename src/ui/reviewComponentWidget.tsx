61a056a2c4b2e748f83e39a3c10daa88d98a4da1
barteksekula <bartek.sekula@gmail.com>
2019-03-27 22:19:01 +0100

    fixed build

--- src/ui/reviewComponentWidget.tsx
+++ src/ui/reviewComponentWidget.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from 'mobx-react';
 import declare from "dojo/_base/declare";
 import WidgetBase from "dijit/_WidgetBase";
-import ReviewLocationsCollection from "./ReviewLocationsCollection";
+import ReviewLocationsCollection from "./reviewLocationsCollection";
 
 import { stores } from "./reviewStore";
 
