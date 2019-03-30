87ec0f8b9a11e6eb82e6acd5e6b41814ab23f8c6
gregwiechec <gwiechec@o2.pl>
2019-03-30 23:48:40 +0100

    Added resources store

--- src/ui/reviewComponentWidget.tsx
+++ src/ui/reviewComponentWidget.tsx
@@ -4,11 +4,13 @@ import { Provider } from 'mobx-react';
 import declare from "dojo/_base/declare";
 import WidgetBase from "dijit/_WidgetBase";
 import ReviewLocationsCollection from "./reviewLocationsCollection";
+import res from "epi/i18n!epi/cms/nls/reviewcomponent";
 
 import { stores } from "./reviewStore";
 
 //TODO: async
 stores.reviewStore.load();
+stores.resources = res;
 
 export default declare([WidgetBase], {
     postCreate: function () {
