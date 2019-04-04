182d1930a2fbbdb835b75f5a5d9b59f3422466c9
barteksekula <bartek.sekula@gmail.com>
2019-04-04 19:57:16 +0200

    reload points after context change

--- src/alloy/modules/_protected/episerver-addons.Reviews/1.0.0/Scripts/onPageEditingInitializer.js
+++ src/alloy/modules/_protected/episerver-addons.Reviews/1.0.0/Scripts/onPageEditingInitializer.js
@@ -1,21 +1,57 @@
 define([
+    "dojo/topic",
+    "dojo/dom-class",
     "epi-cms/contentediting/OnPageEditing",
+    "epi-cms/_ContentContextMixin",
     "alloy-review/ReviewWidget"
 ], function (
+    topic,
+    domClass,
     OnPageEditing,
+    _ContentContextMixin,
     ReviewWidget) {
 
-    function initialize() {
-		var originalPostMixinProperties = OnPageEditing.prototype.postMixInProperties;
-	    OnPageEditing.prototype.postMixInProperties = function () {
-            originalPostMixinProperties.apply(this, arguments);
+    var reviewWidget = null;
 
-            var div = document.createElement("div");
-            var widget = new ReviewWidget({ iframe:this.iFrame.domNode });
-            widget.placeAt(div);
-            this.editLayoutContainer.domNode.appendChild(div);
-	    };
-	}
+    function toggleReviewOverlay(value) {
+        if (reviewWidget) {
+            domClass.toggle(reviewWidget.domNode, "dijitHidden", !value);
+        }
+    }
+
+    function initialize() {
+        //
+        // postMixInProperties
+        //
+
+        topic.subscribe("toggle:reviews", function (toggle) {
+            if (!reviewWidget) {
+                var div = document.createElement("div");
+                var iframe = document.getElementsByName("sitePreview")[0];
+                reviewWidget = new ReviewWidget({ iframe: iframe });
+                reviewWidget.placeAt(div);
+                var editLayoutContainer = document.getElementsByClassName("epi-editorViewport")[0];
+                editLayoutContainer.appendChild(div);
+            }
+            toggleReviewOverlay(toggle);
+        });
+
+        var originalOnReadySetupEditMode = OnPageEditing.prototype.onReadySetupEditMode;
+        OnPageEditing.prototype.onReadySetupEditMode = function (doc) {
+            originalOnReadySetupEditMode.apply(this, arguments);
+            toggleReviewOverlay(false);
+        };
+
+        var originalDestroy = OnPageEditing.prototype.destroy;
+        OnPageEditing.prototype.destroy = function () {
+            originalDestroy.apply(this, arguments);
+            if (reviewWidget) {
+                reviewWidget.destroy();
+                reviewWidget = null;
+            }
+            reviewsVisible = false;
+        };
+    }
 
     return {
         initialize: initialize
