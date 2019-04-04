755e1d60c6ede342125d33e85370365b53d2124a
barteksekula <bartek.sekula@gmail.com>
2019-04-04 20:00:08 +0200

    Toggle Review command
    # set active on the command button on click
    # disable on published content items
    # reset on contextChange

--- src/alloy/modules/_protected/advanced-cms.Reviews/1.0.0/Scripts/ShowReviewCommand.js
+++ src/alloy/modules/_protected/advanced-cms.Reviews/1.0.0/Scripts/ShowReviewCommand.js
@@ -1,27 +1,48 @@
 define([
     "dojo/_base/declare",
     "dojo/topic",
+    "dojo/when",
     "epi/shell/command/_Command",
-    "epi-cms/_ContentContextMixin"
+    "epi-cms/_ContentContextMixin",
+    "epi-cms/contentediting/ContentActionSupport",
 ], function (
     declare,
     topic,
+    when,
     _Command,
-    _ContentContextMixin
+    _ContentContextMixin,
+    ContentActionSupport
 ) {
     return declare([_Command, _ContentContextMixin], {
         name: "ContentReferences",
-        label: "Favourite content",
-        tooltip: "Favourite content",
-        iconClass: 'epi-icon--medium epi-iconStar',
-        canExecute: true,
+        label: "Advanced Review",
+        tooltip: "Toggle advanced review",
+        iconClass: 'epi-icon--medium epi-iconBubble',
+        canExecute: false,
+        reviewEnabled: false,
 
-        _execute: function () {
-            topic.publish("toggle:reviews");
+        constructor: function () {
+            this._toggleCanExecute();
         },
 
-        contentContextChanged: function (content) {
-            //TODO: load comments for changed content?
+        contentContextChanged: function () {
+            this.reviewEnabled = false;
+            this.set("active", false);
+            this._toggleCanExecute();
+            topic.publish("toggle:reviews", this.reviewEnabled);
+        },
+
+        _execute: function () {
+            this.reviewEnabled = !this.reviewEnabled;
+            topic.publish("toggle:reviews", this.reviewEnabled);
+            this.set("active", this.reviewEnabled);
+            this.set('label', this.reviewEnabled ? "Turn off Advanced Review" : "Turn on Advanced Review");
+        },
+
+        _toggleCanExecute: function () {
+            return when(this.getCurrentContent()).then(function (content) {
+                this.set({ canExecute: content.status !== ContentActionSupport.versionStatus.Published });
+            }.bind(this));
         }
     });
 });
