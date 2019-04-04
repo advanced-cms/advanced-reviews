15d5ab502cf6e63a408617c71b31ee6407b15c65
gregwiechec <gwiechec@o2.pl>
2019-04-04 19:39:31 +0200

    changed styles for review points
    * use new icons
    * descrease font size
    * changed colors

--- src/ui/priorityIconMappings.ts
+++ src/ui/priorityIconMappings.ts
@@ -1,8 +1,8 @@
 import { Priority } from "./reviewStore";
 
 const icons = {};
-icons[Priority.Important] = "error_outline";
+icons[Priority.Important] = "priority_high";
 icons[Priority.Normal] = "assignment";
-icons[Priority.Trivial] = "low_priority";
+icons[Priority.Trivial] = "transit_enterexit";
 
 export default icons;
