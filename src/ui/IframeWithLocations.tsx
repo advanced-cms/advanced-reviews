d42bf01769d06d28d7972b528fb01ff8e1f58271
barteksekula <bartek.sekula@gmail.com>
2019-04-05 22:17:31 +0200

    Change dialog state management
    * dialog should manage its own state and only callback
    to its parent if save action was triggered.
    * hide page navigator when adding a new review location

--- src/ui/IframeWithLocations.tsx
+++ src/ui/IframeWithLocations.tsx
@@ -1,10 +1,10 @@
-import React, { FunctionComponent, useState } from 'react';
+import React from 'react';
 import { observer, inject } from 'mobx-react';
 import { IReviewComponentStore, ReviewLocation } from './reviewStore';
-import ReviewEditorDialog from "./dialog/reviewEditorDialog";
+import ReviewEditorDialog, {ReviewDialogState} from "./dialog/reviewEditorDialog";
 import IframeOverlay from "./iframeOverlay";
 import ReviewLocationsCollection from "./reviewLocationsCollection";
-import ReviewsSlidingPanel from './reviews-sliding-panel/reviews-sliding-panel' 
+import ReviewsSlidingPanel from './reviews-sliding-panel/reviews-sliding-panel'
 
 interface IframeState {
     currentLocation: ReviewLocation;
@@ -28,8 +28,8 @@ export default class IFrameWithLocations extends React.Component<IframeWithLocat
     }
 
     showReview(incrementBy: number): void {
-        const { reviewLocations, dialog } = this.props.reviewStore!;
-        let reviewIndex = reviewLocations.indexOf(dialog.currentEditLocation) + incrementBy;
+        const { reviewLocations } = this.props.reviewStore!;
+        let reviewIndex = reviewLocations.indexOf(this.state.currentLocation) + incrementBy;
         if (reviewIndex >= reviewLocations.length) {
             reviewIndex = 0;
         } else if (reviewIndex < 0) {
@@ -38,19 +38,15 @@ export default class IFrameWithLocations extends React.Component<IframeWithLocat
         this.setState({
             currentLocation: reviewLocations[reviewIndex]
         });
-        dialog.showDialog(this.state.currentLocation);
     }
 
     showDialog(location: ReviewLocation): void {
-        const { dialog } = this.props.reviewStore!;
         this.setState({
             currentLocation: location
         });
-
-        dialog.showDialog(location);
     }
 
-    onCloseDialog(action: string): void {
+    onCloseDialog(action: string, state: ReviewDialogState): void {
         if (action !== "save") {
             this.setState({
                 currentLocation: null
@@ -58,8 +54,8 @@ export default class IFrameWithLocations extends React.Component<IframeWithLocat
             return;
         }
 
-        const { saveDialog } = this.props.reviewStore!;
-        saveDialog().then(() => {
+        const { save } = this.props.reviewStore!;
+        save(state, this.state.currentLocation).then(() => {
             this.setState({
                 currentLocation: null
             });
@@ -73,13 +69,15 @@ export default class IFrameWithLocations extends React.Component<IframeWithLocat
         return (<IframeOverlay iframe={this.props.iframe} reviewLocationCreated={(location) => this.setState({ currentLocation: location })}>
             <ReviewLocationsCollection onLocationClick={this.showDialog.bind(this)} />
             <ReviewsSlidingPanel onEditClick={this.showDialog.bind(this)} />
-            <ReviewEditorDialog
-                iframe={this.props.iframe}
-                isDialogOpen={!!this.state.currentLocation}
-                onPrevClick={() => this.showReview(-1)}
-                onNextClick={() => this.showReview(1)}
-                onCloseDialog={(action) => this.onCloseDialog(action)}
-            />
+            {this.state.currentLocation &&
+                <ReviewEditorDialog
+                    currentEditLocation={this.state.currentLocation}
+                    iframe={this.props.iframe}
+                    onPrevClick={() => this.showReview(-1)}
+                    onNextClick={() => this.showReview(1)}
+                    onCloseDialog={(action, state) => this.onCloseDialog(action, state)}
+                />
+            }
         </IframeOverlay>);
     }
 };
