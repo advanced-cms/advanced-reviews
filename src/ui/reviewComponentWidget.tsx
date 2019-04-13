47990d4e31cb8dfc1004a2e1bde117b73bdddda1
barteksekula <bartek.sekula@gmail.com>
2019-04-13 19:19:39 +0200

    Pass the current username to the store

--- src/ui/reviewComponentWidget.tsx
+++ src/ui/reviewComponentWidget.tsx
@@ -4,6 +4,7 @@ import { Provider } from "mobx-react";
 import declare from "dojo/_base/declare";
 import topic from "dojo/topic";
 import WidgetBase from "dijit/_WidgetBase";
+import ApplicationSettings from "epi-cms/ApplicationSettings";
 import _ContentContextMixin from "epi-cms/_ContentContextMixin";
 import ReviewService from "alloy-review/advancedReviewService";
 import IframeWithLocations from "./IframeWithLocations";
@@ -18,6 +19,7 @@ export default declare([WidgetBase, _ContentContextMixin], {
         this.own(this._reviewService);
         this.stores = createStores(this._reviewService, res);
         this.stores.reviewStore.load();
+        this.stores.reviewStore.currentUser = ApplicationSettings.userName;
         this.stores.reviewStore.currentLocale = this.language;
 
         ReactDOM.render(
