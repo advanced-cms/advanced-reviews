4f0aa37c96254891ed4929989a3ff1369abd15c6
barteksekula <bartek.sekula@gmail.com>
2019-04-25 17:19:31 +0200

    Style filters
    Match the filters by colors of the pins so it's easier to
    understand what you are filtering.
    Changed checkboxes to toggles.

--- src/ui/reviews-sliding-panel/reviews-sliding-panel.tsx
+++ src/ui/reviews-sliding-panel/reviews-sliding-panel.tsx
@@ -1,30 +1,27 @@
 import React from "react";
-import { observer, inject } from 'mobx-react';
-import { IReviewComponentStore, ReviewLocation } from './../reviewStore';
-import Checkbox from "@material/react-checkbox";
-import MaterialIcon from '@material/react-material-icon';
-import IconButton from '@material/react-icon-button';
+import { observer, inject } from "mobx-react";
+import { IReviewComponentStore, ReviewLocation } from "../reviewStore";
+import MaterialIcon from "@material/react-material-icon";
+import IconButton from "@material/react-icon-button";
+import Switch from "@material/react-switch";
+import List, { ListItem, ListItemText } from "@material/react-list";
 
-import "@material/react-checkbox/index.scss";
-import List, {
-    ListItem,
-    ListItemText,
-} from "@material/react-list";
-import './reviews-sliding-panel.scss';
+import "@material/react-switch/index.scss";
+import "./reviews-sliding-panel.scss";
 
 interface SlidingPanelProps {
-    reviewStore?: IReviewComponentStore,
-    onEditClick(reviewLocation: ReviewLocation): void
+    reviewStore?: IReviewComponentStore;
+    onEditClick(reviewLocation: ReviewLocation): void;
 }
 
-@inject('reviewStore')
+@inject("reviewStore")
 @observer
 export default class SlidingPanel extends React.Component<SlidingPanelProps, any> {
     constructor(props: SlidingPanelProps) {
         super(props);
         this.state = {
-            panelVisible: true,
-        }
+            panelVisible: true
+        };
     }
 
     onSelected(index: number): void {
@@ -45,56 +42,71 @@ export default class SlidingPanel extends React.Component<SlidingPanelProps, any
                 <IconButton className="panel-toggle" onClick={() => this.setState({ panelVisible: true })}>
                     <MaterialIcon icon="format_list_bulleted" />
                 </IconButton>
-                {this.state.panelVisible &&
+                {this.state.panelVisible && (
                     <div className="panel-container">
                         <div>
-                            <div>
-                                <label className='filter'>
-                                    <Checkbox
-                                        nativeControlId="showReviewLocations"
-                                        checked={filter.showPoints}
-                                        onChange={e => filter.showPoints = !filter.showPoints}
-                                    />
-                                    <span>Show reviews</span>
-                                </label>
+                            <div className="filter main-filter">
+                                <Switch
+                                    nativeControlId="modeSwitcher"
+                                    checked={filter.showPoints}
+                                    onChange={() => (filter.showPoints = !filter.showPoints)}
+                                />
+                                <label htmlFor="modeSwitcher">Display Review Overlay</label>
 
-                                <IconButton className="close-panel" onClick={() => this.setState({ panelVisible: false })}>
+                                <IconButton
+                                    className="close-panel"
+                                    onClick={() => this.setState({ panelVisible: false })}
+                                >
                                     <MaterialIcon icon="close" />
                                 </IconButton>
                             </div>
-                            <div className="filters-container">
-                                <label className='filter'>
-                                    <Checkbox
-                                        nativeControlId="doneTasks"
-                                        checked={filter.showOnlyOpenTasks}
-                                        onChange={e => filter.showOnlyOpenTasks = !filter.showOnlyOpenTasks}
-                                    />
-                                    <span>Only open tasks</span>
-                                </label>
-                                <label className='filter'>
-                                    <Checkbox
-                                        nativeControlId="newTasks"
-                                        checked={filter.showOnlyNewTasks}
-                                        onChange={e => filter.showOnlyNewTasks = !filter.showOnlyNewTasks}
-                                    />
-                                    <span>Only new tasks</span>
-                                </label>
-                            </div>
+                            {filter.showPoints && (
+                                <div className="type-filters">
+                                    <div className="filter unread">
+                                        <Switch
+                                            nativeControlId="showCustom"
+                                            checked={filter.showUnread}
+                                            onChange={() => (filter.showUnread = !filter.showUnread)}
+                                        />
+                                        <label htmlFor="showCustom">Show Unread</label>
+                                    </div>
+                                    <div className="filter active">
+                                        <Switch
+                                            nativeControlId="showActive"
+                                            checked={filter.showActive}
+                                            onChange={() => (filter.showActive = !filter.showActive)}
+                                        />
+                                        <label htmlFor="showActive">Show Active</label>
+                                    </div>
+                                    <div className="filter resolved">
+                                        <Switch
+                                            nativeControlId="showResolved"
+                                            checked={filter.showResolved}
+                                            onChange={() => (filter.showResolved = !filter.showResolved)}
+                                        />
+                                        <label htmlFor="showResolved">Show Resolved</label>
+                                    </div>
+                                </div>
+                            )}
                         </div>
                         <div>
                             <List singleSelection handleSelect={this.onSelected} className="locations">
-                                {reviewLocations.map((location, index) =>
+                                {reviewLocations.map((location, index) => (
                                     <ListItem key={location.id}>
                                         <ListItemText primaryText={location.displayName} />
-                                        <IconButton className="edit" onClick={(e) => this.onEditClick(e, location)} title="Edit" >
+                                        <IconButton
+                                            className="edit"
+                                            onClick={e => this.onEditClick(e, location)}
+                                            title="Edit"
+                                        >
                                             <MaterialIcon icon="edit" />
                                         </IconButton>
                                     </ListItem>
-                                )}
+                                ))}
                             </List>
                         </div>
                     </div>
-                }
+                )}
             </>
         );
     }
