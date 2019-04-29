011d954ca0c240f3427494b05f9116d85bc3813e
gregwiechec <gwiechec@o2.pl>
2019-04-29 21:45:49 +0200

    Added preview links and editable links

--- src/external-reviews/ReviewLinksRepository/ExternalReviewLinkExtension.cs
+++ src/external-reviews/ReviewLinksRepository/ExternalReviewLinkExtension.cs
@@ -8,5 +8,19 @@ namespace AdvancedExternalReviews.ReviewLinksRepository
         {
             return externalReviewLink.ValidTo < DateTime.Now;
         }
+
+        public static bool IsEditableLink(this ExternalReviewLink externalReviewLink)
+        {
+            return externalReviewLink != null &&
+                   !externalReviewLink.IsExpired() &&
+                   externalReviewLink.IsEditable;
+        }
+
+        public static bool IsPreviewableLink(this ExternalReviewLink externalReviewLink)
+        {
+            return externalReviewLink != null &&
+                   !externalReviewLink.IsExpired() &&
+                   !externalReviewLink.IsEditable;
+        }
     }
 }
