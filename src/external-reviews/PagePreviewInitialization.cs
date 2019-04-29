025b2c4eb6b14e8600dd7feeda58c7234fe19957
gregwiechec <gwiechec@o2.pl>
2019-04-29 21:31:32 +0200

    External reviews
    initial commit

---
+++ src/external-reviews/PagePreviewInitialization.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Routing;
+using EPiServer;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Web.Routing;
+
+namespace AdvancedExternalReviews
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class PreviewRouterInitialization : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            var locator = context.Locate.Advanced;
+            var partialRouter = new PagePreviewPartialRouter
+            (
+                locator.GetInstance<IContentLoader>()
+            );
+            RouteTable.Routes.RegisterPartialRouter(partialRouter);
+        }
+
+        void IInitializableModule.Uninitialize(InitializationEngine context) { }
+    }
+}
---
+++ src/external-reviews/ReviewLinksRepository/ExternalReviewStore.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Web.Mvc;
+using EPiServer.Core;
+using EPiServer.Shell.Services.Rest;
+
+namespace AdvancedExternalReviews.ReviewLinksRepository
+{
+    [RestStore("externalreviewstore")]
+    public class ApprovalReviewStore : RestControllerBase
+    {
+        private readonly IExternalReviewLinksRepository _externalReviewLinksRepository;
+        private readonly object _lock = new object();
+
+        public ApprovalReviewStore(IExternalReviewLinksRepository externalReviewLinksRepository)
+        {
+            _externalReviewLinksRepository = externalReviewLinksRepository;
+        }
+
+        [HttpGet]
+        public ActionResult Get(ContentReference id)
+        {
+            return Rest(_externalReviewLinksRepository.GetLinksForContent(id));
+        }
+
+        [HttpPost]
+        public ActionResult Post(PostExternalReviewLinkModel externalLink)
+        {
+            if (externalLink == null || externalLink.ContentLink == null)
+            {
+                return new RestStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            var result = _externalReviewLinksRepository.AddLink(externalLink.ContentLink, externalLink.IsEditable);
+            return Rest(result);
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(string id)
+        {
+            _externalReviewLinksRepository.DeleteLink(id);
+            return Rest(true);
+        }
+    }
+
+    public class PostExternalReviewLinkModel
+    {
+        public ContentReference ContentLink { get; set; }
+        public bool IsEditable { get; set; }
+    }
+}
