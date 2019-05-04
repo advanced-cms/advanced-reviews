c8e7c6a64d0c483b093ef82bdace304b8e1af40a
barteksekula <bartek.sekula@gmail.com>
2019-05-04 18:15:00 +0200

    boyscout: run new npm command without it the edit mode fails

--- site.cmd
+++ site.cmd
@@ -1,2 +1,3 @@
 CALL npm run build:widget --prefix react-components
+CALL npm run build:external-review-widget --prefix react-components
 CALL "C:\Program Files\IIS Express\iisexpress.exe" /path:%~dp0src\Alloy.Mvc.Template
