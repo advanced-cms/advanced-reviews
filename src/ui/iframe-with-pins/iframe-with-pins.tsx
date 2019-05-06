5ff02ba29e05396894299c056917b6da36e70792
barteksekula <bartek.sekula@gmail.com>
2019-05-06 23:24:48 +0200

    Restructure solution
    Component per folder
    storybook inside component folder
    use 'hyphen' file naming
    clean up npm scripts
    renamed reviewLocation to pin

---
+++ src/ui/iframe-with-pins/iframe-with-pins.tsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { observer, inject } from "mobx-react";
+import { IReviewComponentStore, PinLocation, NewPinDto } from "../store/review-store";
+import NewReviewDialog from "../new-review-dialog/new-review-dialog";
+import IframeOverlay from "../iframe-overlay/iframe-overlay";
+import PinCollection from "../pin-collection/pin-collection";
+import ReviewsSlidingPanel from "../reviews-sliding-panel/reviews-sliding-panel";
+import { Snackbar } from "@material/react-snackbar";
+import "@material/react-snackbar/index.scss";
+
+interface IframeState {
+    newLocation: PinLocation;
+}
+
+interface IframeWithLocationsProps {
+    iframe: HTMLIFrameElement;
+    reviewStore?: IReviewComponentStore;
+}
+
+@inject("reviewStore")
+@observer
+export default class IframeWithPins extends React.Component<IframeWithLocationsProps, IframeState> {
+    constructor(props: IframeWithLocationsProps) {
+        super(props);
+
+        this.state = {
+            newLocation: null
+        };
+    }
+
+    onCloseDialog(action: string, state: NewPinDto): void {
+        if (action !== "save") {
+            this.setState({
+                newLocation: null
+            });
+            return;
+        }
+
+        const { save } = this.props.reviewStore!;
+        save(state, this.state.newLocation)
+            .then(() => {
+                this.setState({
+                    newLocation: null
+                });
+            })
+            .catch(e => {
+                //TODO: handle server exceptions
+                alert(e.message);
+            });
+
+        //TODO: show screenshot after save this.setState({ isScreenshotMode: true });
+    }
+
+    onIntroClose = (reason): void => {
+        if (reason !== "action") {
+            return;
+        }
+        //TODO: Set profile value
+        alert("Save profile value");
+    };
+
+    render() {
+        return (
+            <>
+                {this.props.reviewStore.filter.reviewMode && (
+                    <IframeOverlay
+                        iframe={this.props.iframe}
+                        reviewLocationCreated={location => this.setState({ newLocation: location })}
+                    >
+                        <PinCollection newLocation={this.state.newLocation} />
+                        {this.props.reviewStore.reviewLocations.length === 0 && (
+                            <Snackbar
+                                timeoutMs={10000}
+                                onClose={this.onIntroClose}
+                                message="You are now in content review mode. Click on text to create new review entry."
+                                actionText="Do not show this again"
+                                stacked={true}
+                            />
+                        )}
+                    </IframeOverlay>
+                )}
+                <ReviewsSlidingPanel iframe={this.props.iframe} />
+                {this.state.newLocation && (
+                    <NewReviewDialog
+                        currentEditLocation={this.state.newLocation}
+                        iframe={this.props.iframe}
+                        onCloseDialog={(action, state) => this.onCloseDialog(action, state)}
+                    />
+                )}
+            </>
+        );
+    }
+}
--- src/ui/priorityIconMappings.ts
+++
@@ -1,8 +0,0 @@
-import { Priority } from "./reviewStore";
-
-const icons = {};
-icons[Priority.Important] = "priority_high";
-icons[Priority.Normal] = "assignment";
-icons[Priority.Trivial] = "transit_enterexit";
-
-export default icons;
