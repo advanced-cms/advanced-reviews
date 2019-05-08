d9669b2645cb154e2b181282904d41cb614d604b
gregwiechec <gwiechec@o2.pl>
2019-05-08 19:39:20 +0200

    Allow to have external reviews for blocks

---
+++ src/external-reviews/ExternalReview.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace AdvancedExternalReviews
+{
+    public static class ExternalReview
+    {
+        public static bool IsInExternalReviewContext
+        {
+            get
+            {
+                var isInExternalReviewMode = HttpContext.Current.Items["IsInExternalRviewMode"];
+                return (isInExternalReviewMode is bool b) && b;
+            }
+            set => HttpContext.Current.Items["IsInExternalRviewMode"] = value;
+        }
+    }
+}
