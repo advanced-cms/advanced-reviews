48fae1b02896a71ee43404f8fd227c61c6f12dcd
barteksekula <bartek.sekula@gmail.com>
2019-05-13 22:14:47 +0200

    Fix screenshots in EPi
    Remove unnecessary resolve for the iframe, screenshotdialog should
    receive a fully loaded Iframe ref in its props.
    The prop will now only accept IFrameElement, not IFrameElement | string
    The consumer needs to take care to load the ScreenshotDialog once
    the iframe is ready (example in storybook how to do such thing).

--- src/ui/screenshot-dialog/screenshot-dialog.stories.tsx
+++ src/ui/screenshot-dialog/screenshot-dialog.stories.tsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { storiesOf } from "@storybook/react";
 import ScreenshotDialog from "./screenshot-dialog";
 import { action } from "@storybook/addon-actions";
 
-storiesOf("Screenshot picker", module).add("default", () => {
+function Component() {
+    const [anchorElement, setAnchorElement] = useState(null);
+    const [iframeLoaded, setIframeLoaded] = useState(false);
+
+    const onIframeLoaded = () => {
+        setIframeLoaded(true);
+    };
+
     return (
-        <>
-            <iframe id="iframe" style={{ width: "100%", height: "985px" }} src="../.storybook/fake_OPE.html" />
-            <ScreenshotDialog
-                maxWidth={500}
-                maxHeight={300}
-                iframe="iframe"
-                onImageSelected={action("image selected")}
-                toggle={() => {}}
-            />
-        </>
+        <div>
+            <div
+                id="iframeWrapper"
+                style={{
+                    width: "100%",
+                    height: "800px",
+                    position: "absolute",
+                    top: "0",
+                    overflowY: "scroll",
+                    overflowX: "auto"
+                }}
+            >
+                <iframe
+                    id="iframe"
+                    onLoad={onIframeLoaded}
+                    ref={setAnchorElement}
+                    style={{ width: "100%", height: "985px" }}
+                    src="../.storybook/fake_OPE.html"
+                />
+            </div>
+
+            {!!anchorElement && iframeLoaded && (
+                <ScreenshotDialog
+                    maxWidth={500}
+                    maxHeight={300}
+                    iframe={anchorElement}
+                    onImageSelected={action("image selected")}
+                    toggle={() => {}}
+                />
+            )}
+        </div>
     );
+}
+
+storiesOf("Screenshot picker", module).add("default", () => {
+    return <Component />;
 });
