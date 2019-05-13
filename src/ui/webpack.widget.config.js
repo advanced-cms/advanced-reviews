95a59a9a8008611acad6b267e5cd73281ede69c7
barteksekula <bartek.sekula@gmail.com>
2019-05-13 23:00:16 +0200

    Fix mode === "prodcution" | "development" in webpack

--- src/ui/webpack.widget.config.js
+++ src/ui/webpack.widget.config.js
@@ -1,28 +1,30 @@
-var path = require("path");
-var webpackCommon = require("./webpack.config.common");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const path = require("path");
 
-webpackCommon.entry = "./review-component-widget/review-component-widget.tsx";
+module.exports = (env, argv) => {
+    const webpackCommon = require("./webpack.config.common")(env, argv);
+    const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-webpackCommon.output = {
-    filename: "ReviewWidget.js",
-    libraryTarget: "amd",
-    libraryExport: "default",
-    path: path.resolve(__dirname, "../src/Alloy.Mvc.Template/modules/_protected/alloy.Reviews/1.0.0/Scripts")
+    webpackCommon.entry = "./review-component-widget/review-component-widget.tsx";
+
+    webpackCommon.output = {
+        filename: "ReviewWidget.js",
+        libraryTarget: "amd",
+        libraryExport: "default",
+        path: path.resolve(__dirname, "../src/Alloy.Mvc.Template/modules/_protected/alloy.Reviews/1.0.0/Scripts")
+    };
+
+    if (!webpackCommon.plugins) {
+        webpackCommon.plugins = [];
+        webpackCommon.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: "disabled" }));
+    }
+
+    webpackCommon.externals = [
+        "dojo/_base/declare",
+        "dijit/_WidgetBase",
+        "epi/i18n!epi/cms/nls/reviewcomponent",
+        "epi-cms/ApplicationSettings",
+        "epi-cms/_ContentContextMixin",
+        "alloy-review/advancedReviewService"
+    ];
+    return webpackCommon;
 };
-
-if (!webpackCommon.plugins) {
-    webpackCommon.plugins = [];
-    webpackCommon.plugins.push(new BundleAnalyzerPlugin({ analyzerMode: "disabled" }));
-}
-
-webpackCommon.externals = [
-    "dojo/_base/declare",
-    "dijit/_WidgetBase",
-    "epi/i18n!epi/cms/nls/reviewcomponent",
-    "epi-cms/ApplicationSettings",
-    "epi-cms/_ContentContextMixin",
-    "alloy-review/advancedReviewService"
-];
-
-module.exports = webpackCommon;
