99044f755a30f9de9764f40073de199a5e12e32f
gregwiechec <gwiechec@o2.pl>
2019-06-03 02:01:56 +0200

    Style review icon

--- src/alloy/modules/_protected/alloy.Reviews/1.0.0/Scripts/commandsProvider.js
+++ src/alloy/modules/_protected/alloy.Reviews/1.0.0/Scripts/commandsProvider.js
@@ -18,7 +18,7 @@ define([
                 {
                     showLabel: false,
                     widget: ToggleButton,
-                    'class': 'epi-mediumButton'
+                    'class': 'epi-mediumButton epi-review-button'
                 });
         }
     });
