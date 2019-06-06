84f577ee71cba9df8c7edce02074048d4191a3cd
barteksekula <bartek.sekula@gmail.com>
2019-06-06 23:20:33 +0200

    Calculate the offset to the TopLeft of the property
    In case the pin was dropped onto a property we would prefer to position
    the pin based on that property's coordinates.
    We need to calculate the original property's offset to its parent
    parent document and then rescale accordingly.

--- src/ui/position-calculator/position-calculator.ts
+++ src/ui/position-calculator/position-calculator.ts
@@ -1,17 +1,44 @@
-import { Dimensions, PinLocation } from "../store/review-store";
+import { Dimensions, PinPositioningDetails } from "../store/review-store";
 
 export default class PositionCalculator {
-    private readonly _iframe: HTMLIFrameElement;
+    private readonly _documentSize: Dimensions;
+    private _document: Document;
 
-    constructor(iframe: HTMLIFrameElement) {
-        this._iframe = iframe;
+    constructor(documentSize: Dimensions, document?: Document) {
+        this._documentSize = documentSize;
+        this._document = document;
     }
 
-    calculate(location: PinLocation): Dimensions {
-        if (!this._iframe || !location.documentSize) {
-            return location.documentRelativePosition;
+    private rescale(location: PinPositioningDetails): Dimensions {
+        const xFactor = location.documentRelativePosition.x / location.documentSize.x;
+        const yFactor = location.documentRelativePosition.y / location.documentSize.y;
+
+        return {
+            x: xFactor * this._documentSize.x,
+            y: yFactor * this._documentSize.y
+        };
+    }
+
+    calculate(location: PinPositioningDetails): Dimensions {
+        if (location.propertyName && location.propertyPosition && location.propertySize && this._document) {
+            const originalOffsetFromPropertyLeft = location.documentRelativePosition.x - location.propertyPosition.x;
+            const originalOffsetFromPropertyTop = location.documentRelativePosition.y - location.propertyPosition.y;
+
+            const propertyNode: HTMLElement = this._document.querySelector(
+                `[data-epi-property-name='${location.propertyName}']`
+            );
+            if (propertyNode) {
+                const currentOffsetFromPropertyLeft = propertyNode.offsetLeft;
+                const currentOffsetFromPropertyTop = propertyNode.offsetTop;
+
+                return {
+                    x: currentOffsetFromPropertyLeft + originalOffsetFromPropertyLeft,
+                    y: currentOffsetFromPropertyTop + originalOffsetFromPropertyTop
+                };
+            }
         }
 
+        //TODO: rescale in a smart way? return this.rescale(location);
         return location.documentRelativePosition;
     }
 }
