d646ab82e58d28f5d657efbace0be378f1c39ea2
barteksekula <bartek.sekula@gmail.com>
2019-06-09 23:38:33 +0200

    Fix nuget packaging
    * Add web.config transform with custom location tags and protected
    modules
    * Fix versioning of nuget content assets, all versions for both
    server and client should be read from version.cs

--- pack.ps1
+++ pack.ps1
@@ -1,17 +1,25 @@
-msbuild /p:Configuration=Release
+$defaultVersion="1.0.0"
+function ZipCurrentModule
+{
+    Param ([String]$moduleName)
+    Robocopy.exe $defaultVersion\ $version\ /S
+    ((Get-Content -Path module.config -Raw) -Replace $defaultVersion, $version ) | Set-Content -Path module.config
+    7z a "$moduleName.zip" $version Views module.config
+    git checkout module.config
+    Remove-Item $version -Force -Recurse
+}
+
 $fullVersion=[System.Reflection.Assembly]::LoadFrom("src\alloy\bin\AdvancedApprovalReviews.dll").GetName().Version
-echo $fullVersion
 $version="$($fullVersion.major).$($fullVersion.minor).$($fullVersion.build)"
-Write-Host Creating nuget with $version
-cd src\alloy\modules\_protected\episerver-addons.ExternalReviews
-Robocopy.exe 1.0.0\ $version\ /S
-7z a episerver-addons.ExternalReviews.zip $version Views module.config
-Remove-Item $version -force -recurse
-cd ..\episerver-addons.Reviews
-Robocopy.exe 1.0.0\ $version\ /S
-7z a episerver-addons.Reviews.zip $version Views module.config
-Remove-Item $version -force -recurse
-cd ..\..\..\
+Write-Host "Creating nuget with $version version"
+
+Set-Location src\alloy\modules\_protected\episerver-addons.ExternalReviews
+ZipCurrentModule -moduleName episerver-addons.ExternalReviews
+
+Set-Location ..\episerver-addons.Reviews
+ZipCurrentModule -moduleName episerver-addons.Reviews
+
+Set-Location ..\..\..\
 nuget pack advanced-reviews.nuspec -Version $version
-cd ..\..\
-mv src\alloy\advanced-reviews.$version.nupkg advanced-reviews.$version.nupkg
+Set-Location ..\..\
+Move-Item src\alloy\advanced-reviews.$version.nupkg advanced-reviews.$version.nupkg -Force
