edba8dfe1a43b31bb07713f844782b0684c21559
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2019-06-20 13:57:08 +0200

    Check if HttpContext.Current is not null
    closes #48

--- src/external-reviews/ExternalReview.cs
+++ src/external-reviews/ExternalReview.cs
@@ -9,6 +9,10 @@ namespace AdvancedExternalReviews
             //TODO: check token
             get
             {
+                if (HttpContext.Current == null)
+                {
+                    return false;
+                }
                 var isInExternalReviewMode = HttpContext.Current.Items["IsInExternalRviewMode"];
                 return (isInExternalReviewMode is bool b) && b;
             }
