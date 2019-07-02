cd1ebfcce9bfb8426d699516a99455311e809939
barteksekula <bartek.sekula@gmail.com>
2019-07-02 14:31:35 +0200

    boyscout: remove unused parameter

--- src/external-reviews/EditReview/PageEditPartialRouter.cs
+++ src/external-reviews/EditReview/PageEditPartialRouter.cs
@@ -42,7 +42,7 @@ namespace AdvancedExternalReviews.EditReview
             {
                 return null;
             }
-            
+
             // HttpContext.Current.Request.RequestContext.SetContextMode(ContextMode.Edit);
 
             try
@@ -55,7 +55,7 @@ namespace AdvancedExternalReviews.EditReview
 
                 return page;
             }
-            catch (ContentNotFoundException e)
+            catch
             {
                 return null;
             }
