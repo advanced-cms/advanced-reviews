9579ac8ff5b4d3c375a50b5c93167f8d17427072
barteksekula <bartek.sekula@gmail.com>
2019-07-04 17:25:12 +0200

    updated version to 0.2.1

--- version.cs
+++ version.cs
@@ -4,4 +4,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("advanced-cms")]
 [assembly: AssemblyProduct("advanced-reviews")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.2.1.0")]
