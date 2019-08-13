0a3e6562b8806592b70ab1e8f759d0c18efbbbf3
barteksekula <bartek.sekula@gmail.com>
2019-08-13 14:07:59 +0200

    v0.3.0

--- version.cs
+++ version.cs
@@ -4,4 +4,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("advanced-cms")]
 [assembly: AssemblyProduct("advanced-reviews")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
