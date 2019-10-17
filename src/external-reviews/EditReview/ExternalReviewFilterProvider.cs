75e3a5de61067a74dadc27d73d89b546c167ed35
barteksekula <bartek.sekula@gmail.com>
2019-10-17 21:17:47 +0200

    Node DataToken cannot be set to a draft version
    Regression after #64. When setting the token to a draft version
    it is not possible to render the page (access denied).
    But the token is needed because otherwise it is always the StartPage
    that is set in currentContentContext.Content in ContentPropertiesStack.
    This fix will make sure that the Authorize filter will not be used when
    the request is in external review context.
    
    Closes #71
    CLoses #74
    Closes #53

---
+++ src/external-reviews/EditReview/ExternalReviewFilterProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer.Web.Mvc;
+
+namespace AdvancedExternalReviews.EditReview
+{
+    public class ExternalReviewFilterProvider : IFilterProvider
+    {
+        private readonly FilterProviderCollection _filterProviders;
+        private readonly Type _authorizeContent = typeof(AuthorizeContentAttribute);
+
+        public ExternalReviewFilterProvider(IList<IFilterProvider> filters)
+        {
+            _filterProviders = new FilterProviderCollection(filters);
+        }
+
+        public IEnumerable<Filter> GetFilters(ControllerContext controllerContext,
+            ActionDescriptor actionDescriptor)
+        {
+            var filters = _filterProviders.GetFilters(controllerContext, actionDescriptor);
+            return ExternalReview.IsInExternalReviewContext ? filters.Where(x => x.Instance.GetType() != _authorizeContent) : filters;
+        }
+    }
+}
