9d2e9d337ac61a639dd97c886bb2eb7eb18c9da5
barteksekula <bartek.sekula@gmail.com>
2019-11-19 17:51:15 +0100

    Rescale saved coordinates based on the new document size

--- src/ui/src/position-calculator/position-calculator.test.ts
+++ src/ui/src/position-calculator/position-calculator.test.ts
@@ -2,12 +2,12 @@ import PositionCalculator from "./position-calculator";
 import { Dimensions, PinPositioningDetails } from "../store/review-store";
 
 const originalPinLocation: Dimensions = { x: 100, y: 200 };
-const originalOverlaySize: Dimensions = { x: 800, y: 800 };
+const originalDocumentLocation: Dimensions = { x: 800, y: 800 };
 
 const getPin = (extraProps?): PinPositioningDetails => {
     const pin = {
         documentRelativePosition: originalPinLocation,
-        documentSize: originalOverlaySize
+        documentSize: originalDocumentLocation
     };
     Object.assign(pin, extraProps || {});
     return pin;
@@ -17,14 +17,30 @@ describe("when property position data is not available", () => {
     let pin: PinPositioningDetails;
     let positionCalculator: PositionCalculator;
 
-    beforeEach(() => {
-        pin = getPin();
-        positionCalculator = new PositionCalculator(originalOverlaySize);
+    describe("and the document size is the same as the saved one", () => {
+        beforeEach(() => {
+            pin = getPin();
+            positionCalculator = new PositionCalculator(originalDocumentLocation);
+        });
+
+        test("it should return original position", () => {
+            const dimensions = positionCalculator.calculate(pin);
+            expect(dimensions).toStrictEqual(originalPinLocation);
+        });
     });
 
-    test("it should return position relative to the document", () => {
-        const dimensions = positionCalculator.calculate(pin);
-        expect(dimensions).toBe(originalPinLocation);
+    describe("and the document size is different then the saved one", () => {
+        beforeEach(() => {
+            pin = getPin();
+            const newDocumentSize: Dimensions = { x: 1200, y: 600 };
+            positionCalculator = new PositionCalculator(newDocumentSize);
+        });
+
+        test("it should return rescaled position relative to the new document size", () => {
+            const dimensions = positionCalculator.calculate(pin);
+            const expectedPinLocation: Dimensions = { x: 150, y: 150 };
+            expect(dimensions).toStrictEqual(expectedPinLocation);
+        });
     });
 });
 
@@ -40,7 +56,7 @@ describe("when property position data is not available", () => {
 //             propertyPosition: { x: 1, y: 1 },
 //             propertySize: { x: 1, y: 1 }
 //         });
-//         positionCalculator = new PositionCalculator(originalOverlaySize);
+//         positionCalculator = new PositionCalculator(originalDocumentLocation);
 //     });
 //
 //     test("it should return position relative to the document", () => {
