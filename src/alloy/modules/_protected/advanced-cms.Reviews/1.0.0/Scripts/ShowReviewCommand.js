3aecb4095c63095ca44c8542286c50d326b6cd9a
barteksekula <bartek.sekula@gmail.com>
2019-11-21 19:54:28 +0100

    Show Toggle Review only for contentdata items

--- src/alloy/modules/_protected/advanced-cms.Reviews/1.0.0/Scripts/ShowReviewCommand.js
+++ src/alloy/modules/_protected/advanced-cms.Reviews/1.0.0/Scripts/ShowReviewCommand.js
@@ -26,9 +26,9 @@ define([
         reviewEnabled: false,
 
         constructor: function () {
-            this._toggleIsAvailable();
+            this._toggleCanExecute();
 
-            this.own(topic.subscribe("toggle:reviews", function (reviewEnabled) {
+            this.own(topic.subscribe("reviews:toggle", function (reviewEnabled) {
                 this.set("active", reviewEnabled);
                 this.set('label', reviewEnabled ? res.labelenabled : res.labelnotenabled);
             }.bind(this)));
@@ -37,34 +37,36 @@ define([
         contextChanged: function () {
             this.reviewEnabled = false;
             this.set("active", false);
-            this._toggleIsAvailable();
+            this._toggleCanExecute();
             this._publishTopic();
         },
 
         _execute: function () {
             this.reviewEnabled = !this.reviewEnabled;
-            this._publishTopic();
+            when(editorDisplayLanguageResolver.resolve()).then(function (language) {
+                topic.publish("reviews:initialize", this.reviewEnabled, language);
+            }.bind(this));
         },
 
         _publishTopic: function () {
-            when(editorDisplayLanguageResolver.resolve()).then(function (language) {
-                topic.publish("toggle:reviews", this.reviewEnabled, language);
-            }.bind(this));
-        },
-
-        _toggleIsAvailable: function () {
-            return when(this.getCurrentContext()).then(function (context) {
-                if (this._isContentContext(context)) {
-                    this._toggleCanExecute();
-                } else {
-                    this.set({isAvailable: false});
-                }
-            }.bind(this));
+            topic.publish("reviews:toggle", this.reviewEnabled);
         },
 
         _toggleCanExecute: function () {
-            return when(this.getCurrentContent()).then(function (content) {
-                this.set({ isAvailable: true, canExecute: content.status !== ContentActionSupport.versionStatus.Published });
+            return when(this.getCurrentContext()).then(function (context) {
+                if (context.type === "epi.cms.contentdata") {
+                    return when(this.getCurrentContent()).then(function (content) {
+                        this.set({
+                            isAvailable: true,
+                            canExecute: content.status !== ContentActionSupport.versionStatus.Published
+                        });
+                    }.bind(this));
+                } else {
+                    this.set({
+                        isAvailable: false
+                    });
+                }
+
             }.bind(this));
         }
     });
