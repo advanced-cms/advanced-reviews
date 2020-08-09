62ca9a4b44c53fd64d49d91b311cd1c14e9c808c
barteksekula <bartek.sekula@gmail.com>
2020-08-09 11:38:46 +0200

    Make ContentArea.FilteredItems return unpublished content items
    We need a custom PublishedStateAssessorDecorator to behave differently
    when in draft preview mode.

---
+++ src/external-reviews/DraftContentAreaPreview/PublishedStateAssessorDecorator.cs
@@ -0,0 +1,31 @@
+using EPiServer.Core;
+using EPiServer.Globalization;
+
+namespace AdvancedExternalReviews.DraftContentAreaPreview
+{
+    public class PublishedStateAssessorDecorator : IPublishedStateAssessor
+    {
+        private readonly IPublishedStateAssessor _defaultService;
+        private readonly LanguageResolver _languageResolver;
+
+        public PublishedStateAssessorDecorator(IPublishedStateAssessor defaultService, LanguageResolver languageResolver)
+        {
+            _defaultService = defaultService;
+            _languageResolver = languageResolver;
+        }
+
+        public bool IsPublished(IContent content, PublishedStateCondition condition)
+        {
+            if (ExternalReview.IsInExternalReviewContext && ExternalReview.CustomLoaded.Contains(content.ContentLink.ToString()))
+            {
+                var cachedContent = ExternalReview.GetCachedContent(_languageResolver.GetPreferredCulture(), content.ContentLink);
+                if (cachedContent != null)
+                {
+                    return true;
+                }
+            }
+
+            return _defaultService.IsPublished(content, condition);
+        }
+    }
+}
