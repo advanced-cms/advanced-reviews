12b58222103e8a76c51c7fb197c2936ddf8ba7a5
barteksekula <bartek.sekula@gmail.com>
2020-08-12 09:54:25 +0200

    v0.7.3

--- version.cs
+++ version.cs
@@ -4,4 +4,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("advanced-cms")]
 [assembly: AssemblyProduct("advanced-reviews")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.7.2")]
+[assembly: AssemblyVersion("0.7.3")]
