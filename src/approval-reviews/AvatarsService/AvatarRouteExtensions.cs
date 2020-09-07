589901ed6c9ab9f4101c1901a81518991925a618
barteksekula <bartek.sekula@gmail.com>
2020-09-07 15:14:21 +0200

    Fix RouteTable registration
    We need the RouteValueDictionary with the "Controller" name.
    Without it this route will take over all "/" requests.

--- src/approval-reviews/AvatarsService/AvatarRouteExtensions.cs
+++ src/approval-reviews/AvatarsService/AvatarRouteExtensions.cs
@@ -6,7 +6,8 @@ namespace AdvancedApprovalReviews.AvatarsService
     {
         public static void RegisterReviewAvatarsRoute(this RouteCollection routes)
         {
-            var route = new Route("review-avatars/get", new AvatarRouteRouteHandler());
+            var routeData = new RouteValueDictionary {{"Controller", nameof(ReviewAvatarsHandler)}};
+            var route = new Route("review-avatars/get", routeData, new AvatarRouteRouteHandler());
             string[] allowedMethods = { "GET" };
             var methodConstraints = new HttpMethodConstraint(allowedMethods);
             route.Constraints = new RouteValueDictionary { { "httpMethod", methodConstraints } };
