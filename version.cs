94b6cfc93266c4807012fa8efc78a207c5717700
barteksekula <bartek.sekula@gmail.com>
2020-09-08 22:30:17 +0200

    remove unused version.cs

--- version.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyCompany("advanced-cms")]
-[assembly: AssemblyProduct("advanced-reviews")]
-[assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.7.3")]
