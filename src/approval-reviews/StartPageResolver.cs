509610aab93dddf087223b1533dccf95a1b75745
barteksekula <bartek.sekula@gmail.com>
2020-09-24 10:44:00 +0200

    Use existing ISiteDefinitionResolver from Core package
    We don't need to find out the SiteDefinition ourselves because
    there's already an api endpoint for this in Core that returns a
    SiteDefinition instance for a given ContentReference.
    
    #162

--- src/approval-reviews/StartPageResolver.cs
+++ src/approval-reviews/StartPageResolver.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using EPiServer;
-using EPiServer.Core;
+﻿using EPiServer.Core;
 using EPiServer.ServiceLocation;
 using EPiServer.Web;
 using EPiServer.Web.Routing;
@@ -18,30 +14,18 @@ namespace AdvancedApprovalReviews
     public class StartPageUrlResolver: IStartPageUrlResolver
     {
         private readonly UrlResolver _urlResolver;
-        private readonly IContentLoader _contentLoader;
-        private readonly Lazy<IEnumerable<ContentReference>> _startPages;
+        private readonly ISiteDefinitionResolver _siteDefinitionResolver;
 
-        public StartPageUrlResolver(ISiteDefinitionRepository siteDefinitionRepository, UrlResolver urlResolver, IContentLoader contentLoader)
+        public StartPageUrlResolver(UrlResolver urlResolver, ISiteDefinitionResolver siteDefinitionResolver)
         {
             _urlResolver = urlResolver;
-            _contentLoader = contentLoader;
-            _startPages = new Lazy<IEnumerable<ContentReference>>(() =>
-            {
-                return siteDefinitionRepository.List().Select(x => x.StartPage.ToReferenceWithoutVersion());
-            });
+            _siteDefinitionResolver = siteDefinitionResolver;
         }
 
         public string GetUrl(ContentReference contentReference)
         {
-            var startPage = _startPages.Value.FirstOrDefault(x => x == contentReference.ToReferenceWithoutVersion());
-            if (startPage != null)
-            {
-                return _urlResolver.GetUrl(startPage);
-            }
-            var ancestorLinks = _contentLoader.GetAncestors(contentReference)
-                .Select(x => x.ContentLink.ToReferenceWithoutVersion());
-            startPage = _startPages.Value.FirstOrDefault(x => ancestorLinks.Contains(x));
-            return _urlResolver.GetUrl(startPage ?? ContentReference.StartPage);
+            var site = _siteDefinitionResolver.GetByContent(contentReference, true);
+            return _urlResolver.GetUrl(site?.StartPage ?? ContentReference.StartPage);
         }
     }
 }
