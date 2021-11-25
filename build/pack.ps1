90260a61fb582e494958edd95b9ac8f78f3da4a4
barteksekula <bartek.sekula@gmail.com>
2021-11-25 15:16:40 +0100

    Fix packaging

--- build/pack.ps1
+++ build/pack.ps1
@@ -1,15 +1,9 @@
 Param([string]$version, [string] $configuration = "Release")
-$ErrorActionPreference = "Stop"
 $workingDirectory = Get-Location
 $zip = "$workingDirectory\packages\7-Zip.CommandLine\18.1.0\tools\7za.exe"
 
 # Set location to the Solution directory
 (Get-Item $PSScriptRoot).Parent.FullName | Push-Location
-
-Import-Module .\build\exechelper.ps1
-
-# Install .NET tooling
-exec .\build\dotnet-cli-install.ps1
 
 [xml] $versionFile = Get-Content ".\build\dependencies.props"
 # CMS dependency
@@ -18,39 +12,29 @@ $cmsUIVersion = $cmsUINode.InnerText
 $cmsUIParts = $cmsUIVersion.Split(".")
 $cmsUIMajor = [int]::Parse($cmsUIParts[0]) + 1
 $cmsUINextMajorVersion = ($cmsUIMajor.ToString() + ".0.0")
-# Telemetry dependency
-$telemetryNode = $versionFile.SelectSingleNode("Project/PropertyGroup/TelemetryVersion")
-$telemetryVersion = $telemetryNode.InnerText
-$telemetryParts = $telemetryVersion.Split(".")
-$telemetryMajor = [int]::Parse($telemetryParts[0]) + 1
-$telemetryNextMajorVersion = ($telemetryMajor.ToString() + ".0.0")
-# CMS Core dependency
-$cmsCoreNode = $versionFile.SelectSingleNode("Project/PropertyGroup/CmsCoreVersion")
-$cmsCoreVersion = $cmsCoreNode.InnerText
 
 #cleanup all by dtk folder which is used by tests
 Get-ChildItem -Path out\ -Exclude dtk | Remove-Item -Recurse -Force
 
-#copy assets CM
-Copy-Item -Path src\EPiServer.Labs.ContentManager\ClientResources\ -Destination out\episerver-labs-content-manager\$version\ClientResources -recurse -Force
-Copy-Item src\EPiServer.Labs.ContentManager\module.config out\episerver-labs-content-manager
-((Get-Content -Path out\episerver-labs-content-manager\module.config -Raw).TrimEnd() -Replace '=""', "=`"$version`"" ) | Set-Content -Path out\episerver-labs-content-manager\module.config
-Set-Location $workingDirectory\out\episerver-labs-content-manager
-Start-Process -NoNewWindow -Wait -FilePath $zip -ArgumentList "a", "episerver-labs-content-manager.zip", "$version", "module.config"
-
-
-#copy assets from GridView
+#copy assets approval reviews
+Copy-Item -Path src\Advanced.CMS.ApprovalReviews\ClientResources\ -Destination out\advanced-cms-approval-reviews\$version\ClientResources -recurse -Force
+Copy-Item src\Advanced.CMS.ApprovalReviews\module.config out\advanced-cms-approval-reviews
+((Get-Content -Path out\advanced-cms-approval-reviews\module.config -Raw).TrimEnd() -Replace '=""', "=`"$version`"" ) | Set-Content -Path out\advanced-cms-approval-reviews\module.config
+Set-Location $workingDirectory\out\advanced-cms-approval-reviews
+Start-Process -NoNewWindow -Wait -FilePath $zip -ArgumentList "a", "advanced-cms-approval-reviews.zip", "$version", "module.config"
 Set-Location $workingDirectory
-Copy-Item -Path src\EPiServer.Labs.GridView\ClientResources\ -Destination out\episerver-labs-grid-view\$version\ClientResources -recurse -Force
-Copy-Item src\EPiServer.Labs.GridView\module.config out\episerver-labs-grid-view
-((Get-Content -Path out\episerver-labs-grid-view\module.config -Raw).TrimEnd() -Replace '=""', "=`"$version`"" ) | Set-Content -Path out\episerver-labs-grid-view\module.config
-Set-Location $workingDirectory\out\episerver-labs-grid-view
-Start-Process -NoNewWindow -Wait -FilePath $zip -ArgumentList "a", "episerver-labs-grid-view.zip", "$version", "module.config"
 
-Set-Location $workingDirectory\
-exec "dotnet" "publish --no-restore --no-build -c $configuration src\\Alloy.Sample\\Alloy.Sample.csproj"
+#copy assets external reviews
+Copy-Item -Path src\Advanced.CMS.ExternalReviews\ClientResources\ -Destination out\advanced-cms-external-reviews\$version\ClientResources -recurse -Force
+Copy-Item src\Advanced.CMS.ExternalReviews\module.config out\advanced-cms-external-reviews
+((Get-Content -Path out\advanced-cms-external-reviews\module.config -Raw).TrimEnd() -Replace '=""', "=`"$version`"" ) | Set-Content -Path out\advanced-cms-external-reviews\module.config
+Set-Location $workingDirectory\out\advanced-cms-external-reviews
+Start-Process -NoNewWindow -Wait -FilePath $zip -ArgumentList "a", "advanced-cms-external-reviews.zip", "$version", "module.config"
+Set-Location $workingDirectory
 
 # Packaging public packages
-exec "dotnet" "pack --no-restore --no-build -c $configuration /p:PackageVersion=$version /p:CmsCoreVersion=$cmsCoreVersion /p:CmsUIVersion=$cmsUIVersion /p:CmsUINextMajorVersion=$cmsUINextMajorVersion /p:TelemetryVersion=$telemetryVersion /p:TelemetryNextMajorVersion=$telemetryNextMajorVersion EPiServer.Labs.ContentManager.sln"
+echo "AAAAAAAA"
+echo $version
+dotnet pack --no-restore --no-build -c $configuration /p:PackageVersion=$version /p:CmsUIVersion=$cmsUIVersion /p:CmsUINextMajorVersion=$cmsUINextMajorVersion Advanced.CMS.AdvancedReviews.sln
 
 Pop-Location
--- pack.cmd
+++ pack.cmd
@@ -1,2 +1,11 @@
-CALL build.cmd Release
-powershell ./pack.ps1
+@ECHO OFF
+SETLOCAL
+
+SET CONFIGURATION=Debug
+
+IF "%2"=="Release" (SET CONFIGURATION=Release)
+
+@REM CALL build.cmd Release
+powershell ./build/pack.ps1  -version %1 -configuration %CONFIGURATION%
+
+EXIT /B %errorlevel%
