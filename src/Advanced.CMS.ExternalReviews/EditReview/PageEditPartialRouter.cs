c9779e6cd705fc293845dbc3ed40966c797c531d
barteksekula <bartek.sekula@gmail.com>
2022-10-12 15:30:26 +0200

    Adjust _contentLanguageAccessor.Language to the routed language
    Closes #221

--- src/Advanced.CMS.ExternalReviews/EditReview/PageEditPartialRouter.cs
+++ src/Advanced.CMS.ExternalReviews/EditReview/PageEditPartialRouter.cs
@@ -4,7 +4,6 @@ using EPiServer.Core.Routing;
 using EPiServer.Core.Routing.Pipeline;
 using EPiServer.ServiceLocation;
 using EPiServer.Web.Routing;
-using Microsoft.AspNetCore.Http;
 
 namespace Advanced.CMS.ExternalReviews.EditReview
 {
@@ -14,18 +13,18 @@ namespace Advanced.CMS.ExternalReviews.EditReview
         private readonly IExternalReviewLinksRepository _externalReviewLinksRepository;
         private readonly ExternalReviewOptions _externalReviewOptions;
         private readonly ProjectContentResolver _projectContentResolver;
-        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IContentLanguageAccessor _contentLanguageAccessor;
         private readonly ExternalReviewState _externalReviewState;
 
         public PageEditPartialRouter(IExternalReviewLinksRepository externalReviewLinksRepository,
             ExternalReviewOptions externalReviewOptions,
-            ProjectContentResolver projectContentResolver, IHttpContextAccessor httpContextAccessor,
+            ProjectContentResolver projectContentResolver, IContentLanguageAccessor contentLanguageAccessor,
             ExternalReviewState externalReviewState)
         {
             _externalReviewLinksRepository = externalReviewLinksRepository;
             _externalReviewOptions = externalReviewOptions;
             _projectContentResolver = projectContentResolver;
-            _httpContextAccessor = httpContextAccessor;
+            _contentLanguageAccessor = contentLanguageAccessor;
             _externalReviewState = externalReviewState;
         }
 
@@ -60,12 +59,7 @@ namespace Advanced.CMS.ExternalReviews.EditReview
             _externalReviewState.IsEditLink = true;
             _externalReviewState.Token = token;
 
-            if (externalReviewLink.VisitorGroups != null)
-            {
-                //TODO: is it used?
-                _httpContextAccessor.HttpContext.Items["ImpersonatedVisitorGroupsById"] =
-                    externalReviewLink.VisitorGroups;
-            }
+            _contentLanguageAccessor.Language = content.Language;
 
             try
             {
--- src/Advanced.CMS.ExternalReviews/PagePreviewPartialRouter.cs
+++ src/Advanced.CMS.ExternalReviews/PagePreviewPartialRouter.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 using Advanced.CMS.ExternalReviews.PinCodeSecurity;
 using Advanced.CMS.ExternalReviews.ReviewLinksRepository;
 using EPiServer.Core;
@@ -7,63 +6,9 @@ using EPiServer.Core.Routing;
 using EPiServer.Core.Routing.Pipeline;
 using EPiServer.ServiceLocation;
 using EPiServer.Web.Routing;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 
 namespace Advanced.CMS.ExternalReviews
 {
-    public class TestMiddleware
-    {
-        private readonly RequestDelegate _next;
-        private readonly ExternalReviewState _externalReviewState;
-        private readonly IExternalLinkPinCodeSecurityHandler _externalLinkPinCodeSecurityHandler;
-        private readonly IExternalReviewLinksRepository _externalReviewLinksRepository;
-
-        public TestMiddleware(RequestDelegate next, ExternalReviewState externalReviewState,
-            IExternalLinkPinCodeSecurityHandler externalLinkPinCodeSecurityHandler,
-            IExternalReviewLinksRepository externalReviewLinksRepository)
-        {
-            _next = next;
-            _externalReviewState = externalReviewState;
-            _externalLinkPinCodeSecurityHandler = externalLinkPinCodeSecurityHandler;
-            _externalReviewLinksRepository = externalReviewLinksRepository;
-        }
-
-        public async Task InvokeAsync(HttpContext context)
-        {
-            try
-            {
-                await _next(context);
-            }
-            catch (Exception ex)
-            {
-                await HandleExceptionAsync(context, ex);
-            }
-        }
-
-        private Task HandleExceptionAsync(HttpContext context, Exception exception)
-        {
-            if (exception is PinAuthenticationFailedException)
-            {
-                context.Response.Redirect("/account/login");
-            }
-
-            return Task.CompletedTask;
-        }
-    }
-
-    public class PinAuthenticationFailedException : Exception
-    {
-    }
-
-    public static class TestMiddlewareExtensions
-    {
-        public static IApplicationBuilder UseTestMiddleware(this IApplicationBuilder builder)
-        {
-            return builder.UseMiddleware<TestMiddleware>();
-        }
-    }
-
     /// <summary>
     /// Partial router used to display readonly version of the page
     /// </summary>
@@ -74,20 +19,20 @@ namespace Advanced.CMS.ExternalReviews
         private readonly IExternalLinkPinCodeSecurityHandler _externalLinkPinCodeSecurityHandler;
         private readonly IExternalReviewLinksRepository _externalReviewLinksRepository;
         private readonly ExternalReviewOptions _externalReviewOptions;
-        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IContentLanguageAccessor _contentLanguageAccessor;
         private readonly ExternalReviewState _externalReviewState;
 
         public PagePreviewPartialRouter(IExternalReviewLinksRepository externalReviewLinksRepository,
             ExternalReviewOptions externalReviewOptions,
             ProjectContentResolver projectContentResolver,
             IExternalLinkPinCodeSecurityHandler externalLinkPinCodeSecurityHandler,
-            IHttpContextAccessor httpContextAccessor, ExternalReviewState externalReviewState)
+            IContentLanguageAccessor contentLanguageAccessor, ExternalReviewState externalReviewState)
         {
             _externalReviewLinksRepository = externalReviewLinksRepository;
             _externalReviewOptions = externalReviewOptions;
             _projectContentResolver = projectContentResolver;
             _externalLinkPinCodeSecurityHandler = externalLinkPinCodeSecurityHandler;
-            _httpContextAccessor = httpContextAccessor;
+            _contentLanguageAccessor = contentLanguageAccessor;
             _externalReviewState = externalReviewState;
         }
 
@@ -115,6 +60,8 @@ namespace Advanced.CMS.ExternalReviews
                 return null;
             }
 
+            _contentLanguageAccessor.Language = content.Language;
+
             nextSegment = segmentContext.GetNextSegment(nextSegment.Remaining);
             var token = nextSegment.Next.ToString();
 
@@ -126,12 +73,6 @@ namespace Advanced.CMS.ExternalReviews
 
             _externalReviewState.Token = token;
             _externalReviewState.ProjectId = externalReviewLink.ProjectId;
-            //TODO: is it used?
-            if (externalReviewLink.VisitorGroups != null)
-            {
-                _httpContextAccessor.HttpContext.Items["ImpersonatedVisitorGroupsById"] =
-                    externalReviewLink.VisitorGroups;
-            }
 
             // PIN code security check, if user is not authenticated, then redirect to login page
             if (!_externalLinkPinCodeSecurityHandler.UserHasAccessToLink(externalReviewLink))
