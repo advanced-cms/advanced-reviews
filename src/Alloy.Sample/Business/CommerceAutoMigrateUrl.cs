6abce829a899ce5d472bc37ef31a2e451eccf882
barteksekula <bartek.sekula@gmail.com>
2022-11-28 12:46:00 +0100

    Add default routing to Catalog items

--- src/Alloy.Sample/Business/CommerceAutoMigrateUrl.cs
+++ src/Alloy.Sample/Business/CommerceAutoMigrateUrl.cs
@@ -4,24 +4,23 @@ using EPiServer.Framework.Initialization;
 using EPiServer.ServiceLocation;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace Alloy.Sample.Business
+namespace Alloy.Sample.Business;
+
+[ModuleDependency(typeof(MigrationInitializationModule))]
+public class CommerceAutoMigrateUrlInitializer : IConfigurableModule
 {
-    [ModuleDependency(typeof(MigrationInitializationModule))]
-    public class CommerceAutoMigrateUrlInitializer : IConfigurableModule
+    public void ConfigureContainer(ServiceConfigurationContext context)
     {
-        public void ConfigureContainer(ServiceConfigurationContext context)
-        {
-            context.Services.RemoveAll(typeof(MigrateActionUrlResolver));
-            context.Services.AddSingleton((MigrateActionUrlResolver) (
-                ( _,  action) => $"EPiServer/Commerce/Migrate/{action}"));
-        }
+        context.Services.RemoveAll(typeof(MigrateActionUrlResolver));
+        context.Services.AddSingleton((MigrateActionUrlResolver) (
+            ( _,  action) => $"EPiServer/Commerce/Migrate/{action}"));
+    }
 
-        public void Initialize(InitializationEngine context)
-        {
-        }
+    public void Initialize(InitializationEngine context)
+    {
+    }
 
-        public void Uninitialize(InitializationEngine context)
-        {
-        }
+    public void Uninitialize(InitializationEngine context)
+    {
     }
 }
---
+++ src/Alloy.Sample/Business/SiteInitialization.cs
@@ -0,0 +1,26 @@
+﻿using EPiServer.Commerce.Initialization;
+using EPiServer.Commerce.Routing;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+
+namespace Alloy.Sample.Business;
+
+[ModuleDependency(typeof(InitializationModule))]
+public class SiteInitialization : IConfigurableModule
+{
+    public void ConfigureContainer(ServiceConfigurationContext context)
+    {
+
+    }
+
+    public void Initialize(InitializationEngine context)
+    {
+        CatalogRouteHelper.MapDefaultHierarchialRouter(false);
+    }
+
+    public void Uninitialize(InitializationEngine context)
+    {
+
+    }
+}
