80fd66e96d501ecf644bfc4d732282be4ce559a5
barteksekula <bartek.sekula@gmail.com>
2023-05-03 18:53:05 +0200

    boyscout: do not use deprecated method

--- src/Alloy.Sample/Business/Rendering/AlloyContentAreaRenderer.cs
+++ src/Alloy.Sample/Business/Rendering/AlloyContentAreaRenderer.cs
@@ -45,7 +45,7 @@ namespace Alloy.Sample.Business.Rendering
 
         private static string GetTypeSpecificCssClasses(ContentAreaItem contentAreaItem, IContentRepository contentRepository)
         {
-            var content = contentAreaItem.GetContent();
+            var content = contentAreaItem.LoadContent();
             var cssClass = content == null ? String.Empty : content.GetOriginalType().Name.ToLowerInvariant();
 
             var customClassContent = content as ICustomCssInContentArea;
