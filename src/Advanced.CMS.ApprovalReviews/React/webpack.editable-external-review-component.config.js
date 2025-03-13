4981b75a8d6eff7a455ff09dacf1222c7ac6ab9e
Bartosz Sekula <Bartosz.Sekula@optimizely.com>
2025-03-13 10:00:47 +0100

    move webpack configs to root folder

--- src/Advanced.CMS.ApprovalReviews/React/webpack.editable-external-review-component.config.js
+++ src/Advanced.CMS.ApprovalReviews/React/webpack.editable-external-review-component.config.js
@@ -1,13 +1,13 @@
 var path = require("path");
 
 module.exports = (env, argv) => {
-    const webpackCommon = require("../../webpack.config.common")(env, argv);
+    const webpackCommon = require("./webpack.config.common")(env, argv);
 
     webpackCommon.entry = "./src/editable-external-reviews/editable-external-review-component.tsx";
 
     webpackCommon.output = {
         filename: "external-review-component.js",
-        path: path.resolve(__dirname, "../../../../Advanced.CMS.ExternalReviews/ClientResources")
+        path: path.resolve(__dirname, "../../Advanced.CMS.ExternalReviews/ClientResources"),
     };
 
     return webpackCommon;
--- src/Advanced.CMS.ApprovalReviews/React/webpack.external-review-manage-links.widget.config.js
+++ src/Advanced.CMS.ApprovalReviews/React/webpack.external-review-manage-links.widget.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = (env, argv) => {
-    const webpackCommon = require("../../webpack.config.common")(env, argv);
+    const webpackCommon = require("./webpack.config.common")(env, argv);
 
     webpackCommon.entry = "./src/external-reviews-manage-links/external-review-manage-links-widget.tsx";
 
@@ -9,7 +9,7 @@ module.exports = (env, argv) => {
         filename: "external-review-manage-links-component.js",
         libraryTarget: "amd",
         libraryExport: "default",
-        path: path.resolve(__dirname, "../../../ClientResources")
+        path: path.resolve(__dirname, "../ClientResources"),
     };
 
     webpackCommon.externals = [
@@ -17,7 +17,7 @@ module.exports = (env, argv) => {
         "dijit/_WidgetBase",
         "epi/i18n!epi/cms/nls/externalreviews",
         "epi-cms/_ContentContextMixin",
-        "advanced-cms-external-reviews/external-review-service"
+        "advanced-cms-external-reviews/external-review-service",
     ];
 
     return webpackCommon;
