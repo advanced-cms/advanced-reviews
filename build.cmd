b294afd09e031673dca7a5840e7b2ca0f278e740
Bartosz Sekula <Bartosz.Sekula@optimizely.com>
2025-10-07 13:24:53 +0200

    Turn off EOL warning
    net6 is out of support already

--- build.cmd
+++ build.cmd
@@ -5,7 +5,7 @@ IF "%1"=="Release" (set CONFIGURATION=Release) ELSE (set CONFIGURATION=Debug)
 ECHO Building in %CONFIGURATION%
 
 REM Build the C# solution.
-CALL dotnet build -c %CONFIGURATION%
+CALL dotnet build -c %CONFIGURATION% /p:CheckEolTargetFramework=false
 IF %errorlevel% NEQ 0 EXIT /B %errorlevel%
 
 REM Build client side
