ab9ecfeabe19dc83b5bdc2da51e9012c3951f857
Bartosz Sekula <Bartosz.Sekula@optimizely.com>
2025-10-07 12:47:30 +0200

    clean up csproj, use c# 12

--- src/Advanced.CMS.ExternalReviews/BlocksPreview/BlockPreviewInitialization.cs
+++ src/Advanced.CMS.ExternalReviews/BlocksPreview/BlockPreviewInitialization.cs
@@ -1,27 +1,29 @@
-﻿using EPiServer.Framework;
-using EPiServer.Framework.Initialization;
+﻿//TODO NETCORE
 
-namespace Advanced.CMS.ExternalReviews.BlocksPreview;
-
-/// <summary>
-/// Module for support external blocks rendering.
-/// </summary>
-[ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
-internal class BlockPreviewInitialization : IInitializableModule
-{
-    public void Initialize(InitializationEngine context)
-    {
-        // context.Locate.TemplateResolver()
-        //     .TemplateResolved += BlocksTemplateCoordinator.OnTemplateResolved;
-    }
-
-    public void Uninitialize(InitializationEngine context)
-    {
-        // ServiceLocator.Current.GetInstance<TemplateResolver>()
-        //     .TemplateResolved -= BlocksTemplateCoordinator.OnTemplateResolved;
-    }
-
-    public void Preload(string[] parameters)
-    {
-    }
-}
+// using EPiServer.Framework;
+// using EPiServer.Framework.Initialization;
+//
+// namespace Advanced.CMS.ExternalReviews.BlocksPreview;
+//
+// /// <summary>
+// /// Module for support external blocks rendering.
+// /// </summary>
+// [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+// internal class BlockPreviewInitialization : IInitializableModule
+// {
+//     public void Initialize(InitializationEngine context)
+//     {
+//         // context.Locate.TemplateResolver()
+//         //     .TemplateResolved += BlocksTemplateCoordinator.OnTemplateResolved;
+//     }
+//
+//     public void Uninitialize(InitializationEngine context)
+//     {
+//         // ServiceLocator.Current.GetInstance<TemplateResolver>()
+//         //     .TemplateResolved -= BlocksTemplateCoordinator.OnTemplateResolved;
+//     }
+//
+//     public void Preload(string[] parameters)
+//     {
+//     }
+// }
--- src/Advanced.CMS.ExternalReviews/BlocksPreview/BlocksTemplateCoordinator.cs
+++ src/Advanced.CMS.ExternalReviews/BlocksPreview/BlocksTemplateCoordinator.cs
@@ -1,34 +1,34 @@
-﻿namespace Advanced.CMS.ExternalReviews.BlocksPreview
-{
-    internal class BlocksTemplateCoordinator
-    {
-        //TODO NETCORE
-        // public static void OnTemplateResolved(object sender, TemplateResolverEventArgs args)
-        // {
-        //     var options = ServiceLocator.Current.GetInstance<ExternalReviewOptions>();
-        //     if (!options.IsEnabled)
-        //     {
-        //         return;
-        //     }
-        //
-        //     if (args.ItemToRender is BlockData)
-        //     {
-        //         if (args.RequestedCategory == TemplateTypeCategories.MvcController ||
-        //             args.RequestedCategory == TemplateTypeCategories.Page)
-        //         {
-        //             if (!ExternalReview.IsInExternalReviewContext)
-        //             {
-        //                 return;
-        //             }
-        //
-        //             var blockPreviewController =
-        //                 args.SupportedTemplates.FirstOrDefault(x => x.TemplateType == typeof(BlockPreviewController));
-        //             if (blockPreviewController != null)
-        //             {
-        //                 args.SelectedTemplate = blockPreviewController;
-        //             }
-        //         }
-        //     }
-        // }
-    }
-}
+﻿// namespace Advanced.CMS.ExternalReviews.BlocksPreview
+// {
+//     internal class BlocksTemplateCoordinator
+//     {
+//         //TODO NETCORE
+//         // public static void OnTemplateResolved(object sender, TemplateResolverEventArgs args)
+//         // {
+//         //     var options = ServiceLocator.Current.GetInstance<ExternalReviewOptions>();
+//         //     if (!options.IsEnabled)
+//         //     {
+//         //         return;
+//         //     }
+//         //
+//         //     if (args.ItemToRender is BlockData)
+//         //     {
+//         //         if (args.RequestedCategory == TemplateTypeCategories.MvcController ||
+//         //             args.RequestedCategory == TemplateTypeCategories.Page)
+//         //         {
+//         //             if (!ExternalReview.IsInExternalReviewContext)
+//         //             {
+//         //                 return;
+//         //             }
+//         //
+//         //             var blockPreviewController =
+//         //                 args.SupportedTemplates.FirstOrDefault(x => x.TemplateType == typeof(BlockPreviewController));
+//         //             if (blockPreviewController != null)
+//         //             {
+//         //                 args.SelectedTemplate = blockPreviewController;
+//         //             }
+//         //         }
+//         //     }
+//         // }
+//     }
+// }
