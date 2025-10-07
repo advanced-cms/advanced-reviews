c0dbcb38b946ac75b9abb4206fe41a93f245ede5
Bartosz Sekula <Bartosz.Sekula@optimizely.com>
2025-10-07 13:04:56 +0200

    Improve basic scenario test case
    Do not use strange pattern with scenario builder factory
    Use classfixture

---
+++ test/Advanced.CMS.AdvancedReviews.IntegrationTests.Basic/When_Creating_Token_For_Page_Without_Everyone_Access.cs
@@ -0,0 +1,53 @@
+using System.Net;
+using System.Security.Principal;
+using Advanced.CMS.ExternalReviews.ReviewLinksRepository;
+using EPiServer.Authorization;
+using EPiServer.Security;
+using EPiServer.ServiceLocation;
+using TestSite.Models;
+using Xunit;
+
+namespace Advanced.CMS.AdvancedReviews.IntegrationTests.Basic;
+
+[Collection(IntegrationTestCollection.Name)]
+public class When_Creating_Token_For_Page_Without_Everyone_Access(When_Creating_Token_For_Page_Without_Everyone_Access.TestFixture fixture)
+    : IClassFixture<CommonFixture>, IClassFixture<When_Creating_Token_For_Page_Without_Everyone_Access.TestFixture>
+{
+    public class TestFixture(SiteFixture siteFixture) : IAsyncLifetime
+    {
+        public IContentRepository ContentRepository { get; } = siteFixture.Services.GetInstance<IContentRepository>();
+        public IPrincipalAccessor PrincipalAccessor { get; } = siteFixture.Services.GetInstance<IPrincipalAccessor>();
+        public IServiceProvider Services => siteFixture.Services;
+        public HttpClient Client { get; } = siteFixture.Client;
+
+        public ContentReference TargetFolder { get; private set; }
+        public StandardPage Page { get; private set; }
+        public string InitialUsername = "Bolek";
+        public ExternalReviewLink GeneratedReviewLink { get; set; }
+
+        public async Task InitializeAsync()
+        {
+            PrincipalAccessor.Principal = new GenericPrincipal(
+                new GenericIdentity(InitialUsername),
+                [Roles.Administrators, Roles.CmsAdmins, Roles.CmsEditors, Roles.WebAdmins, Roles.WebEditors]);
+
+            TargetFolder = ContentRepository.CreateTargetFolder();
+            Page = ContentRepository.CreatePage().WithoutEveryoneAccess();
+            GeneratedReviewLink = Page.GenerateExternalReviewLink();
+
+            await Task.CompletedTask;
+        }
+
+        public async Task DisposeAsync() => await ContentRepository.CleanupAsync(Page);
+    }
+
+    [Fact]
+    public async Task It_Returns_200()
+    {
+        var message = new HttpRequestMessage(HttpMethod.Get, fixture.GeneratedReviewLink.LinkUrl);
+        var response = await fixture.Client.SendAsync(message);
+        await response.Content.ReadAsStringAsync();
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
+
---
+++ test/Advanced.CMS.AdvancedReviews.IntegrationTests/ContentRepositoryTestExtensions.cs
@@ -0,0 +1,89 @@
+﻿using Advanced.CMS.ExternalReviews.ReviewLinksRepository;
+using EPiServer.Security;
+using EPiServer.ServiceLocation;
+using TestSite.Models;
+
+namespace Advanced.CMS.AdvancedReviews.IntegrationTests;
+
+public static class ContentRepositoryTestExtensions
+{
+    private static IContentRepository ContentRepository => ServiceLocator.Current.GetInstance<IContentRepository>();
+
+    private static IPropertyDefinitionRepository PropertyDefinitionRepository =>
+        ServiceLocator.Current.GetInstance<IPropertyDefinitionRepository>();
+
+    private static IContentTypeRepository ContentTypeRepository =>
+        ServiceLocator.Current.GetInstance<IContentTypeRepository>();
+
+    private static IContentSecurityRepository ContentSecurityRepository =>
+        ServiceLocator.Current.GetInstance<IContentSecurityRepository>();
+
+    private static IExternalReviewLinksRepository ExternalReviewLinksRepository =>
+        ServiceLocator.Current.GetInstance<IExternalReviewLinksRepository>();
+
+    public static StandardPage CreatePage(this IContentRepository repo, string name = null)
+    {
+        var page = repo.GetDefault<StandardPage>(ContentReference.StartPage);
+        page.PageName = name ?? Guid.NewGuid().ToString();
+        repo.Save(page, AccessLevel.NoAccess);
+        return page;
+    }
+
+    public static StandardPage WithoutEveryoneAccess(this StandardPage page)
+    {
+        var accessControlList = new AccessControlList
+        {
+            new AccessControlEntry(EPiServer.Authorization.Roles.CmsAdmins, AccessLevel.Administer)
+        };
+        accessControlList.IsInherited = false;
+        page.SaveSecurityInfo(ContentSecurityRepository, accessControlList, SecuritySaveType.Replace);
+        return page;
+    }
+
+    public static ExternalReviewLink GenerateExternalReviewLink(this StandardPage page, Project project = null)
+    {
+        return ExternalReviewLinksRepository.AddLink(page.ContentLink, false, TimeSpan.FromDays(1), project?.ID);
+    }
+
+    public static StandardPage PublishPage(this StandardPage page)
+    {
+        ContentRepository.Publish(page, AccessLevel.NoAccess);
+        return page;
+    }
+
+    public static ContentReference CreateTargetFolder(this IContentRepository repo)
+    {
+        var folder = repo.GetDefault<ContentFolder>(ContentReference.GlobalBlockFolder);
+        folder.Name = Guid.NewGuid().ToString();
+        repo.Save(folder, AccessLevel.NoAccess);
+        return folder.ContentLink;
+    }
+
+    public static Task CleanupAsync(this IContentRepository repo, IContent page)
+    {
+        repo.Delete(page.ContentLink, true, AccessLevel.NoAccess);
+        foreach (var contentType in ContentTypeRepository.List())
+        {
+            ResetContentType(contentType);
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private static void ResetContentType(ContentType contentType)
+    {
+        var writableContentType = contentType.CreateWritableClone() as ContentType;
+        foreach (var property in writableContentType.PropertyDefinitions)
+        {
+            if (property.ExistsOnModel)
+            {
+                continue;
+            }
+
+            PropertyDefinitionRepository.Delete(property);
+        }
+
+        writableContentType.ResetContentType();
+        ContentTypeRepository.Save(writableContentType);
+    }
+}
